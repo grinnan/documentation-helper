afbb8189fdc3004788fd1e5c63b5eb53a91b6f36
ns-cgrinnan <cgrinnan@netskope.com>
2024-09-12 21:38:40 +0000

    Sep 12, 2024, 2:38 PM

---
+++ ingestion.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
